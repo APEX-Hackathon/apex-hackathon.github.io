f0085d730964e4a05f3061ffd75ab51420b55ff4
Gracie <battlesbrown@gmail.com>
2024-11-02 20:04:36 -0500

    Update QNA

--- js/faq.js
+++ js/faq.js
@@ -2,61 +2,61 @@ const faqData = {
     generalInfo: [
         {
             question: "What is a hackathon?",
-            answer: "A hackathon is a 24-hour coding competition where teams work together to create innovative solutions. This event is beginner-friendly and focuses primarily on front-end development, though teams may choose to include backend components.",
+            answer: "The APEX Hackathon is a 24-hour coding competition where participants collaborate in teams to create innovative projects.",
+        },
+        {
+            question: "How does overnight work?",
+            answer: "While code repositories will be closed between midnight and 6 AM, teams may continue planning and brainstorming. We strongly recommend getting adequate rest to maintain productivity throughout the event.",
         },
         {
             question: "Is this event beginner-friendly?",
-            answer: "Yes! This hackathon is designed to be beginner-friendly. While basic programming knowledge is recommended (see our preparation resources), the event focuses primarily on front-end development. Backend development is optional and won't be provided with additional event materials.",
+            answer: "Yes! This hackathon is designed for all skill levels. While basic programming knowledge is recommended, we focus primarily on front-end development. Teams may choose to include backend components, but this is optional and won't have additional event support.",
         },
         {
             question: "How does scoring work?",
-            answer: "Projects are judged on:<ul><li>Awe: Innovation and uniqueness</li><li>Prowess: Technical skill and teamwork</li><li>Entertainment: Creativity and audience appeal</li><li>X Factor: People's Choice award</li></ul>",
+            answer: "Judging criteria:<ul><li>Awe: Does your project do something new, innovative, or outstanding?</li><li>Prowess: Is it clear from your code that you are a strong team - both technically and in the aspect of teamwork and project management?</li><li>Entertainment: Does your project use creativity and uniqueness that capture an audience?</li><li>X: The X award is the Hackathon equivalent to the People's Choice award. The project that receives the most votes from other teams (you cannot vote for your own) will gain bonus points!</li></ul>",
         },
         {
-            question: "What's not allowed at the event?",
-            answer: "For safety reasons, please do not bring:<ul><li>Any foods containing nuts (strict no-nuts policy due to allergies)</li><li>Any disruptive items or behavior</li></ul>",
+            question: "Is there food I should not bring?",
+            answer: "<ul><li>Peanuts, any type of nuts or anything that may contain nuts</li></ul>",
         },
     ],
     timeline: [
         {
             question: "What's the registration deadline?",
-            answer: "Registration must be completed at least 24 hours before the event to ensure team placement. Fill out the registration form at <a href='https://forms.gle/Qw8Rj1tY3ET8jz6q6' target='_blank'>this link</a>.",
-        },
-        {
-            question: "What's the event schedule?",
-            answer: "<ul><li>Friday 4:45 PM - 5:00 PM: Check-in at front office</li><li>Friday 5:30 PM: Official start</li><li>Saturday 6:00 PM: Project judging</li><li>View the full schedule <a href='schedule.html'>here</a></li></ul>",
+            answer: "Registration must be completed at least 24 hours before the event. Teams will be formed one day before the event for participants who haven't already joined a team. Late registrations may not be assigned to a team. Register here: <a href='https://docs.google.com/forms/d/e/1FAIpQLSeRj7b-Gdfw10ahcIEMRpzIvNW2qZGbG8NgqM1GcB5jXW6c0w/viewform?usp=sf_link' target='_blank'>Registration Form</a>",
         },
         {
             question: "Where is the event located?",
-            answer: "Address: 295 SE Ashworth Rd, Waukee, IA 50263<br><br>When arriving at the WILC, check in at the front office to receive a hand stamp verifying your registration. This is only required on Friday - no re-stamping needed on Saturday.",
+            answer: "Address: 295 SE Ashworth Rd, Waukee, IA 50263<br><br>The event will be held at the WILC building. Check-in opens at 4:45 PM on Friday at the front office. The majority of the competition will take place at this location, except for overnight work.",
         },
         {
-            question: "What are the working arrangements?", // TODO when do we close? vvv
-            answer: "You will be able to work at the WILC building until it closes at 9:00 PM. Teams can work either remotely or in person at a chosen location/member's house outside of WILC hours. Code collaboration will be disabled between midnight and 6 AM to ensure participants get rest.",
+            question: "What are the working arrangements?",
+            answer: "During WILC hours, teams will work in the building. Outside of WILC hours, teams can choose to work either remotely or meet in person at an agreed-upon location. The majority of project work will take place at the WILC. Note: Code repositories will be locked between midnight and 6 AM to ensure participants get rest.",
         },
     ],
     gettingReady: [
         {
             question: "What technical skills do I need?",
-            answer: "Basic knowledge of these technologies is highly recommended:<ul><li>HTML/CSS for website visuals</li><li>JavaScript for website mechanics</li><li>Python for server-side code (optional)</li></ul>",
+            answer: "It is HIGHLY recommended that you have basic knowledge of these languages:<ul><li>HTML/CSS for the visuals of the website like text, colors, positioning, etc</li><li>Javascript for the mechanics of the website like calculations, drop-down boxes, etc</li><li>Python for any server-side code like having users that can log in to accounts</li></ul>",
         },
         {
-            question: "What's provided?", // TODO
-            answer: "We provide workspace at the WILC 5-9PM Friday and ???-6PM Saturday with WiFi and outlets. We will also provide a pizza dinner on Friday.",
+            question: "What's provided?",
+            answer: "We are planning on serving pizza on friday. For Saturday, we won't be serving food so make sure to bring lunch! Also, bring snacks if you would like.",
         },
         {
             question: "What should I bring?",
-            answer: "Essential items:<ul><li>Laptop/programming device</li><li>Power supply/charger</li><li>Comfortable clothes</li><li>Snacks (no nuts!)</li><li>Lunch for Saturday</li></ul>Note: Pizza will be provided on Friday evening.",
+            answer: "To bring:<ul><li>Device to program on (participants will be required to bring their device they wish to use, it will not be provided). It is highly recommended to bring a laptop.</li><li>Adequate power supply for your device</li><li>Snacks for yourself or team members (check allergen list)</li><li>Wear comfortable clothes</li></ul>",
         },
         {
             question: "How do I set up my development environment?",
-            answer: "Required tools:<ul><li>GitHub account (accept organization invitation after registration)</li><li>VSCode as your code editor</li><li>Recommended extensions:<ul><li>Git Blame/Git Lens</li><li>Live Server</li><li>Prettier</li><li>Language-specific extensions</li></ul></ul>",
+            answer: "Recommended Tools:<ul><li>VSCode for programming</li><li>Extensions (for VSCode):<ul><li>Git Blame/Git Lens - for tracking who did what</li><li>Live Server - to view website</li><li>Prettier - to format code</li><li>Language-specific extensions - to improve quality of life</li><li>Snippet extensions (pre-made code blocks for free)</li><li>Intellisense for your languages of choice</li></ul></li></ul>",
         },
     ],
     resources: [
         {
             question: "Where can I learn the basics?",
-            answer: "Free video tutorials:<ul><li>HTML: <a href='https://youtu.be/UB1O30fR-EE' target='_blank'>Getting Started with HTML</a></li><li>CSS: <a href='https://youtu.be/yfoY53QXEnI' target='_blank'>CSS Crash Course</a></li><li>JavaScript: <a href='https://youtu.be/hdI2bqOjy3c' target='_blank'>JavaScript Fundamentals</a></li><li>Python: <a href='https://youtu.be/JJmcL1N2KQs' target='_blank'>Python Basics</a></li></ul>",
+            answer: "Free video tutorials:<ul><li>HTML: <a href='https://youtu.be/UB1O30fR-EE?si=L9QipCVUN4SxEIwN' target='_blank'>Getting Started with HTML</a></li><li>CSS: <a href='https://youtu.be/yfoY53QXEnI?si=QuCwl0AGXg1zs9pf' target='_blank'>CSS Crash Course</a></li><li>JavaScript: <a href='https://youtu.be/hdI2bqOjy3c?si=EcKrrhZSP0ey021j' target='_blank'>JavaScript Fundamentals</a></li><li>Python: <a href='https://youtu.be/JJmcL1N2KQs?si=e0srFjRGxotABdSV' target='_blank'>Python Basics</a></li></ul>",
         },
         {
             question: "Where can I practice coding?",
@@ -66,11 +66,11 @@ const faqData = {
     teamFormation: [
         {
             question: "How are teams organized?",
-            answer: "Participants will be randomly assigned a team before the event if they haven't already formed or joined one. The maximum team size is 4 members. If you don't submit the registration form at least 24 hours in advance, you may not be placed on a team.",
+            answer: "Teams can be formed in two ways: you can either join with your chosen teammates or be assigned to a team. Team assignments happen one day before the event. Each team has a maximum of 4 members. Note: You must submit the registration form at least 24 hours before the event to be placed on a team.",
         },
         {
             question: "How does team collaboration work?",
-            answer: "Teams will use GitHub for code sharing, deployment, and project management. Make sure to accept the organization invitation you'll receive after registration.",
+            answer: "In order to collaborate with your team, you'll be using a GitHub repository. This allows you to:<ul><li>Share, edit, and collaborate on the same code with your teammates</li><li>Automatically deploy your website to GitHub Pages or Render</li><li>Keep a history of your project to easily undo</li><li>Save your project on the cloud</li></ul>This allows us to:<ul><li>Lock down repositories during downtime (12am to 6am)</li><li>Moderate and manage teams during the Hackathon</li><li>Verify that code is your own and not AI or plagiarized</li><li>Provide feedback and review on your code</li><li>Help with technical issues</li></ul>First, you'll need to create a GitHub account. You'll supply us with this in the signup form. Once those two steps are completed, you'll receive an email from our GitHub organization APEX-Hackathon asking you to join the Organization and the team you specified in the signup form. If you have not specified a team, you will be placed into one the day before competition. After you get this email, you should accept the invitation and navigate to the repository (code) for your project. Now you can start editing the repository from your own computer, either directly from the web interface or using your code editor.",
         },
     ],
 };
